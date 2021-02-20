e6f663342d004610ee6bd7261774dca087726efb
Maxwell <maxwellherron5@gmail.com>
2021-02-19 18:46:52 -0600

    created command for accessing monster details

--- src/api_utils.py
+++ src/api_utils.py
@@ -21,7 +21,9 @@ LOGGER = logging.getLogger(__name__)
 
 class RequestExecutionError(Exception):
     """Custom exception raised when errors occur during Open5e requests"""
-    pass
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
 
 
 def get_spell_details(spell: str) -> dict:
@@ -32,7 +34,7 @@ def get_spell_details(spell: str) -> dict:
 
     Returns:
         formatted_details (dict): Dictionary representation of all spell details.
-    
+
     Raises:
         RequestExecutionError
     """
@@ -43,8 +45,9 @@ def get_spell_details(spell: str) -> dict:
     details = json.loads(res.data.decode('utf-8'))
     LOGGER.info("GET REQUEST RESPONSE CODE: %d", res.status)
     if res.status != 200:
-        LOGGER.error("Request failed with error message: %s", details['detail'])
-        raise RequestExecutionError
+        error = f"Oops, looks like the database doesn't have an entry for that spell :( not my fault ¯\_(ツ)_/¯"
+        LOGGER.error(error)
+        raise RequestExecutionError(error)
 
     # Trimming unneeded details from result
     junk_keys = ["slug", "document__slug", "document__title", "document__license_url"]
@@ -54,3 +57,49 @@ def get_spell_details(spell: str) -> dict:
     formatted_details = {field.replace("_", " ").capitalize(): details[field] for field in details}
     LOGGER.info(formatted_details)
     return formatted_details
+
+
+def get_monster_details(monster: str) -> dict:
+    """Makes a request to Open5e to access all details on the given monster.
+
+    Params:
+        monster (str): Name of the monster formatted for the GET request.
+
+    Returns:
+        formatted_details (dict): Dictionary representation of all monster details.
+
+    Raises:
+        RequestExecutionError
+    """
+    http = urllib3.PoolManager()
+    endpoint = f"{DND_URL}/monsters/{monster}/"
+    LOGGER.info("Request: %s", endpoint)
+    res = http.request('GET', endpoint)
+    details = json.loads(res.data.decode('utf-8'))
+    LOGGER.info("GET REQUEST RESPONSE CODE: %d", res.status)
+    if res.status != 200:
+        error = f"Oops, looks like the database doesn't have an entry for that monster :( not my fault ¯\_(ツ)_/¯"
+        LOGGER.error(error)
+        raise RequestExecutionError(error)
+
+    # Trimming unneeded details from result
+    junk_keys = ["img_main", "document__slug", "document__title", "document__license_url"]
+    [details.pop(key) for key in junk_keys]
+
+    # Cleaning up field names for a pretty print
+    formatted_details = {field.replace("_", " ").capitalize(): details[field] for field in details}
+
+    # Normalizing list or dictionary attributes into strings
+    normalized_details = {}
+    for field in formatted_details:
+        if isinstance(formatted_details[field], dict):
+            normalized_details[field] = '\n'.join("%s: %s" % tup for tup in formatted_details[field].items())
+            LOGGER.info("Dict converted to %s", normalized_details[field])
+        elif isinstance(formatted_details[field], list):
+            normalized_details[field] = '\n'.join(['\n'.join("%s: %s" % tup for tup in subfield.items()) for subfield in formatted_details[field]])
+            LOGGER.info("List converted to %s", normalized_details[field])
+        else:
+            normalized_details[field] = formatted_details[field]
+
+    LOGGER.info(normalized_details)
+    return normalized_details
--- src/spellbot.py
+++ src/spellbot.py
@@ -13,7 +13,11 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-from api_utils import get_spell_details
+from api_utils import (
+    get_spell_details,
+    get_monster_details,
+    RequestExecutionError
+)
 
 # Creating access to environment variables
 load_dotenv()
@@ -33,28 +37,77 @@ async def on_ready():
     """
     LOGGER.info("%s has connected to the server", bot.user.name)
 
+@bot.command(name="goodbot", help="Tell this bot how good they are")
+async def on_message(ctx):
+    """
+    """
+    await ctx.channel.send(u"thank you \U0001f604")
 
 @bot.command(name="spell", help="Enter the name of your spell and I'll tell you about it! For now, please format as <spell-name>")
 async def on_message(ctx, spell: str):
     """
     """
     LOGGER.info("New message: %s", spell)
-    spell_details = get_spell_details(spell)
+    try:
+        spell_details = get_spell_details(spell)
+        # Formatting the output details into an embedded message
+        msg = discord.Embed(title=f"{spell_details['Name']}", description="Here's all you need to know! (and probably more)")
 
-    # Formatting the output details into an embedded message
-    msg = discord.Embed(title=f"{spell_details['Name']}", description="Here's all you need to know! (and probably more)")
+        # Populating embedded message with all returned fields
+        for field in spell_details:
+            # Setting base value for empty fields
+            if not spell_details[field]:
+                spell_details[field] = "None"
+            LOGGER.info("field --- %s", field)
+            LOGGER.info("value --- %s", spell_details[field])
+            msg.add_field(name=f"**{field}**", value=f"{spell_details[field]}", inline=False)
+        msg.set_footer(text="...anything look weird here? Let me know!")
+        await ctx.channel.send(embed=msg)
 
-    # Populating embedded message with all returned fields
-    for field in spell_details:
-        # Setting base value for empty fields
-        if not spell_details[field]:
-            spell_details[field] = "None"
-        LOGGER.info("field --- %s", field)
-        LOGGER.info("value --- %s", spell_details[field])
-        msg.add_field(name=f"**{field}**", value=f"{spell_details[field]}", inline=False)
-    msg.set_footer(text="...anything look weird here? Let me know!")
+    except RequestExecutionError as exc:
+        msg = exc.message
+        await ctx.channel.send(msg)
 
-    await ctx.channel.send(embed=msg)
+
+@bot.command(name="monster", help="Enter the name of the monster and I'll tell you about it! For now, please format as <monster-name>")
+async def on_message(ctx, monster: str):
+    """
+    """
+    LOGGER.info("New message: %s", monster)
+    try:
+        monster_details = get_monster_details(monster)
+        # Formatting the output details into an embedded message
+        msg = discord.Embed(title=f"{monster_details['Name']}", description="Here's all you need to know! (and probably more)")
+
+        # Populating embedded message with all returned fields
+        for field in monster_details:
+            # Setting base value for empty fields
+            if not monster_details[field]:
+                monster_details[field] = "None"
+            LOGGER.info("field --- %s", field)
+            LOGGER.info("value --- %s", monster_details[field])
+            if isinstance(monster_details[field], dict):
+                msg.add_field(name=f"**{field}**", value=f"All details for field {field} below", inline=False)
+                for key in monster_details[field]:
+                    if len(str(monster_details[field][key])) > 1024:
+                        pass
+                    msg.add_field(name=f"{key}", value=f"{monster_details[field][key]}", inline=False)
+            elif isinstance(monster_details[field], list):
+                msg.add_field(name=f"**{field}**", value=f"All details for field {field} below", inline=False)
+                for item in monster_details[field]:
+                    for tup in item.items():
+                        if len(str(tup[1])) > 1024:
+                            pass
+                        msg.add_field(name=f"**{tup[0]}**", value=f"{tup[1]}", inline=False)
+            else:
+                if len(str(monster_details[field])) <= 1024:
+                    msg.add_field(name=f"**{field}**", value=f"{monster_details[field]}", inline=False)
+        msg.set_footer(text="...anything look weird here? Let me know!")
+        await ctx.channel.send(embed=msg)
+
+    except RequestExecutionError as exc:
+        msg = exc.message
+        await ctx.channel.send(msg)
 
 
 @bot.event
@@ -66,5 +119,6 @@ async def on_error(event, *args, **kwargs):
             f.write(f'Unhandled message: {args[0]}\n')
         else:
             raise
+
 
 bot.run(TOKEN)
